c284a5de4db65a9ca2b408a49c24b8834c0f3dd4
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-08-28 13:28:25 +0100

    created a login page that allows existing user to log in

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-
 function LoginPage() {
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  function handleSubmit(event) {
+    event.preventDefault();
+    const loginData = {
+      email: event.target.addEmail.value,
+      password: event.target.password.value,
+    };
+
+    // sending the data to backend
+    axios
+      .post("http://localhost:8080/auth/login", loginData)
+      .then((response) => {
+        console.log(response);
+        const token = response.data.token;
+        console.log(token);
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.response.data);
+      });
+  }
+
   return (
     <section className="loginPage">
       <section>
@@ -10,7 +36,7 @@ function LoginPage() {
         </div>
       </section>
       <section className="loginPage__card">
-        <form className="loginPage__form">
+        <form className="loginPage__form" onSubmit={handleSubmit}>
           <div className="loginPage__email  loginPage__formGroup">
             <label className="loginPage__userEmailLabel">Email:</label>
             <input
@@ -26,11 +52,17 @@ function LoginPage() {
             <input
               className="loginPage__passwordInput"
               placeholder="Enter your mingle Password"
+              type="password"
+              name="password"
+              id="password"
             />
           </div>
+          <button className="login__button">Log in</button>
+
+          {error && <div className="login__message">{error}</div>}
 
           <div>
-            <Link to= "/signUpPage">
+            <Link to="/signUpPage">
               <p>Don't have an account, press this mysteious button </p>
               <button>Sign up Page</button>
             </Link>
