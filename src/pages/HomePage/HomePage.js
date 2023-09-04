535382281fdb441250c90a8c54cfdb24a8786ae8
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-09-04 21:34:02 +0100

    removing unecessary comments

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,5 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
+import React from "react";
+import { Link } from "react-router-dom"; // Assuming you're using React Router for navigation
 import "./HomePage.scss";
 
 function HomePage() {
@@ -8,18 +7,19 @@ function HomePage() {
     <div className="homePage">
       <header className="homePage__header">
         <h1 className="homePage__title">MingleU</h1>
-        <p className="homePage__catchyLine">Connect with Like-minded Students!</p>
+        <p className="homePage__catchyLine">
+          Connect with Like-minded Students!
+        </p>
       </header>
 
       <section className="homePage__hero">
-        {/* <h2 className="homePage__heroTitle">Welcome to MingleU</h2> */}
-        {/* <p className="homePage__heroDescription">
-          Build connections, find study buddies, and explore shared interests with fellow university students.
-        </p> */}
-
-        <div className='homePage__buttonsWrapper'>
-        <Link to="/loginPage" className="login-link">Log In</Link>
-        <Link to="/signupPage" className="login-link">Sign Up</Link>
+        <div className="homePage__buttonsWrapper">
+          <Link to="/loginPage" className="login-link">
+            Log In
+          </Link>
+          <Link to="/signupPage" className="login-link">
+            Sign Up
+          </Link>
         </div>
       </section>
 
